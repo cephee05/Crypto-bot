c24bbc932d5bfbb04e48efeec7005ddb8ac4365f
cephee05 <159174617+cephee05@users.noreply.github.com>
2025-03-22 23:28:05 +0100

    Update Risk.py

--- BOT/accumulation_distribution_pattern/Risk.py
+++ BOT/accumulation_distribution_pattern/Risk.py
@@ -2,4 +2,26 @@
 condition du killswitch
 MAX DD : 40%
 Capital Minimal : 65%
+
+🔧 Méthode 1 : Levier de Kelly (Optimal Leverage)
+evier optimal selon Kelly = rendement annuel ÷ (volatilité annuelle)²
+
+Avec tes données :
+
+μ = 0,1606
+σ = 0,209
+
+Levier optimal = 0,1606 / (0,209)² ≈ 0,1606 / 0,0437 ≈ 3,67
+
+Ce levier maximise la croissance à long terme, mais il est très agressif. La plupart des traders utilisent entre 0,5 et 0,7 du levier de Kelly.
+
+Donc un levier raisonnable ≈ 1,8
+
+Le Kelly Criterion calculé est 0,1637, ce qui confirme que l'on est actuellement en-dessous du levier optimal théorique, et que nous avons une marge pour augmenter l'exposition.
+​
+
+​
+
+
+
 '''
